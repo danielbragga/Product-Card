adf693e53b9528d393c2a4c90e692cc6d361bd51
Daniel Braga <danielbraga2121@gmail.com>
2022-09-21 21:32:05 -0300

    project-finished

---
+++ src/components/Card/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="content">
+      <div className="card-container">
+        <div className="img">
+          <img src="/images/image-product-desktop.jpg" alt=" " />
+        </div>
+        <div className="card-content">
+          <h6 className="product-category">Perfume</h6>
+          <h2 className="product-name">Gabrielle essence Eau De Parfum</h2>
+          <p className="product-description">
+            A floral, solar and voluptous interpretation composed by Oliver
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <div className="prices">
+            <p className="current-price">$149.99</p>
+            <p className="old-price">$169.99</p>
+          </div>
+          <button className="button">
+            <img src="/images/cart.svg" />
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
